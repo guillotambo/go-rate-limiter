7e09bc515e5da98eca3462fe946c80f72dd00540
Guillermo Tamborindeguy <guillermo.tamborindeguy@mercadolibre.com>
2022-02-06 22:37:44 -0300

    adding test descriptions

--- middleware/rate_limiter_test.go
+++ middleware/rate_limiter_test.go
@@ -32,7 +32,7 @@ func (t *clockMock) nowNano() int64 {
 }
 
 func TestRateLimiter_Accept(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
+	t.Run("Success - User on time", func(t *testing.T) {
 		clMock := createClock(1, 2, 3)
 		rateLimiter := newRateLimiterWithClock(3, 3, &clMock)
 		httpRecorder := httptest.NewRecorder()
@@ -47,7 +47,7 @@ func TestRateLimiter_Accept(t *testing.T) {
 		rateLimiter.Accept(ctx)
 		assert.Equal(t, httpRecorder.Code, http.StatusOK)
 	})
-	t.Run("success", func(t *testing.T) {
+	t.Run("Success - User waiting time window", func(t *testing.T) {
 		clMock := createClock(1, 2)
 		rateLimiter := newRateLimiterWithClock(2, 1, &clMock)
 		httpRecorder := httptest.NewRecorder()
@@ -60,22 +60,7 @@ func TestRateLimiter_Accept(t *testing.T) {
 		rateLimiter.Accept(ctx)
 		assert.Equal(t, httpRecorder.Code, http.StatusOK)
 	})
-	t.Run("fail", func(t *testing.T) {
-		clMock := createClock(1, 2, 3)
-		rateLimiter := newRateLimiterWithClock(2, 3, &clMock)
-		httpRecorder := httptest.NewRecorder()
-		ctx, _ := gin.CreateTestContext(httpRecorder)
-		ctx.Request, _ = http.NewRequestWithContext(ctx, "GET", "/message/", nil)
-		ctx.Request.Header.Add("userId", "pepe")
-
-		rateLimiter.Accept(ctx)
-		assert.Equal(t, httpRecorder.Code, http.StatusOK)
-		rateLimiter.Accept(ctx)
-		assert.Equal(t, httpRecorder.Code, http.StatusOK)
-		rateLimiter.Accept(ctx)
-		assert.Equal(t, httpRecorder.Code, http.StatusTooManyRequests)
-	})
-	t.Run("fail2", func(t *testing.T) {
+	t.Run("Success - Two users on time", func(t *testing.T) {
 		clMock := createClock(1, 1, 2, 2, 3, 3)
 		rateLimiter := newRateLimiterWithClock(4, 5, &clMock)
 		httpRecorder := httptest.NewRecorder()
@@ -99,7 +84,22 @@ func TestRateLimiter_Accept(t *testing.T) {
 		rateLimiter.Accept(ctx2)
 		assert.Equal(t, httpRecorder.Code, http.StatusOK)
 	})
-	t.Run("fail2", func(t *testing.T) {
+	t.Run("Fail - User exceeds rate", func(t *testing.T) {
+		clMock := createClock(1, 2, 3)
+		rateLimiter := newRateLimiterWithClock(2, 3, &clMock)
+		httpRecorder := httptest.NewRecorder()
+		ctx, _ := gin.CreateTestContext(httpRecorder)
+		ctx.Request, _ = http.NewRequestWithContext(ctx, "GET", "/message/", nil)
+		ctx.Request.Header.Add("userId", "pepe")
+
+		rateLimiter.Accept(ctx)
+		assert.Equal(t, httpRecorder.Code, http.StatusOK)
+		rateLimiter.Accept(ctx)
+		assert.Equal(t, httpRecorder.Code, http.StatusOK)
+		rateLimiter.Accept(ctx)
+		assert.Equal(t, httpRecorder.Code, http.StatusTooManyRequests)
+	})
+	t.Run("Fail - First user on time, Second exceeds rate", func(t *testing.T) {
 		clMock := createClock(1, 1, 2, 2, 3, 3, 4)
 		rateLimiter := newRateLimiterWithClock(3, 5, &clMock)
 		httpRecorder := httptest.NewRecorder()
